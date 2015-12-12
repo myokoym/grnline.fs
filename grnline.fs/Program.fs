a5882dc1620ea4027d1facbf34133cb0787aec4e
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-12-12 17:39:43 +0900

    Remove needless comments

--- grnline.fs/Program.fs
+++ grnline.fs/Program.fs
@@ -1,6 +1,4 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
-open Nessos.Argu
+﻿open Nessos.Argu
 open Newtonsoft.Json
 open System.IO
 open System.Diagnostics

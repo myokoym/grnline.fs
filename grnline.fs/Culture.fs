1d129c1ed07364e7c50024e0172a64988a8c37b7
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-12-20 02:18:33 +0900

    Try to remove unnecessary p/invoke

--- grnline.fs/Culture.fs
+++ grnline.fs/Culture.fs
@@ -3,15 +3,6 @@
 open System.Runtime.InteropServices
 open System.Globalization
 
-#if WIN32
-[<DllImport("kernel32.dll")>]
-extern int GetSystemDefaultLCID()
-
-
-let get_codepage = 
-    let lcid = GetSystemDefaultLCID()
-    let ci = CultureInfo(lcid)
+let get_codepage =
+    let ci = CultureInfo.CurrentCulture
     ci.TextInfo.OEMCodePage
-#else
-let get_codepage = 65001 // For *nix like environment, it always assumes UTF-8 for now.
-#endif
